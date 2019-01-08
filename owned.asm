aa1654e60dcd5578baf72066a159f6fd02e28d71
mishrasunny174 <mishrasunny174@gmail.com>
2019-01-09 01:07:42 +0530

    fixed shell code size

--- owned.asm
+++ owned.asm
@@ -10,7 +10,7 @@ _start:
    mov   al, 4		;moving 4 to eax i.e, write op code
    mov   bl, 1		;moving 1 to ebx i.e, fd = 1
    push  0x21212164	;pushing !!!d
-   push  0x656e776f	;pushing ewno ; pushed owned!!! to stack in little endian format
+   push  0x656e776f	;pushing enwo pushed owned!!! to stack in little endian format
    mov   ecx, esp	;moving owned!!! location to ecx
    mov   dl, 8		;moving length of string to edx i.e, 8
    int   0x80		;calling kernel
@@ -18,5 +18,5 @@ _start:
    xor   ebx, ebx	;clearing ebx to return code 0
    int   0x80		;calling system
 
-; char shellcode = "\x31\xc0\x31\xdb\x31\xc9\x31\xd2\xb0\x04\xb3\x01\x68\x64\x21\x21\x21\x68\x64\x6f\x77\x6e\x65\x89\xe1\xb2\x08\xcd\x80\xb0\x01\x31\xdb\xcd\x80"
-; length of shellcode = 36
+; char shellcode = "\x31\xc0\x31\xdb\x31\xc9\x31\xd2\xb0\x04\xb3\x01\x68\x64\x21\x21\x21\x68\x6f\x77\x6e\x65\x89\xe1\xb2\x08\xcd\x80\xb0\x01\x31\xdb\xcd\x80"
+; length of shellcode = 34
