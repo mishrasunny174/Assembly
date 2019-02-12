7e88a8ba17cb51bfce65147c091bbff83c0af2e8
mishrasunny174 <mishrasunny174@gmail.com>
2019-02-12 14:24:56 +0530

    now using stack

--- x86-64/Misc/Loop.asm
+++ x86-64/Misc/Loop.asm
@@ -7,14 +7,16 @@ section  .text
 global  _start
 
 _start:
-	mov rbx, 0x10   ;initializing counter to 10
-begin:  mov rax, 0x1    ;write syscall number
-	mov rdi, 0x1 	;fd to write
-	mov rsi, msg 	;msg to write
-	mov rdx, length ;length of message to write
-	syscall 	;calling system
-	dec rbx   	;dec counter
-	jnz begin       ;jump if not zero flag set
-	mov rax, 0x3c   ;exit syscall number
-	mov rdi, 0x0    ;exit status
-	syscall 	;syscall
+	mov 	rcx, 0x10   	;initializing counter to 10
+begin:  push 	rcx 		;pushing rcx to save it's value
+	mov 	rax, 0x1    	;write syscall number
+	mov 	rdi, 0x1 	;fd to write
+	mov 	rsi, msg 	;msg to write
+	mov 	rdx, length 	;length of message to write
+	syscall 		;calling system
+	pop 	rcx
+	dec 	rcx 	   	;dec counter
+	jnz 	begin       	;jump if not zero flag set
+	mov 	rax, 0x3c   	;exit syscall number
+	mov 	rdi, 0x0    	;exit status
+	syscall 		;syscall
