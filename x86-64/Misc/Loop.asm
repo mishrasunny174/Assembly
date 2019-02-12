e5b8ea4095667930dc94580a673a4ef3f5a3242d
mishrasunny174 <mishrasunny174@gmail.com>
2019-02-12 14:10:02 +0530

    added a simple loop

---
+++ x86-64/Misc/Loop.asm
@@ -0,0 +1,21 @@
+section  .data
+
+msg: 	db	'Hello, world!',0xa
+length: equ	$-msg
+
+section  .text
+
+global  _start
+
+_start:
+	mov rbx, 0x5 	;loop counter
+begin:  mov rax, 0x1    ;write syscall number
+	mov rdi, 0x1 	;fd to write
+	mov rsi, msg 	;msg to write
+	mov rdx, length ;length of message to write
+	syscall 	;calling system
+	dec rbx   	;dec counter
+	jnz begin       ;jump if not zero flag set
+	mov rax, 0x3c   ;exit syscall number
+	mov rdi, 0x0    ;exit status
+	syscall 	;syscall
